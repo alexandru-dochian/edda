8dda37c68e9899d10aa2181bb5b935777e812e1b
Alex Doc <alexandru.dochian@forktex.com>
2023-03-15 21:53:07 +0100

    Finalized 3.b)

--- titanic.R
+++ titanic.R
@@ -1,4 +1,15 @@
 library(tidyverse)
 
 titanic <- read_tsv("resources/titanic.txt")
-summary(titanic)
+
+
+model_age_pclass <- glm(Survived ~ Age:PClass, data = titanic, family = "binomial")
+age_first_class = summary(model_age_pclass)$coefficients["Age:PClass1st", "Pr(>|z|)"]
+age_second_class = summary(model_age_pclass)$coefficients["Age:PClass2nd", "Pr(>|z|)"]
+age_third_class = summary(model_age_pclass)$coefficients["Age:PClass3rd", "Pr(>|z|)"]
+
+
+model_age_sex <- glm(Survived ~ Age:Sex, data = titanic, family = "binomial")
+age_female = summary(model_age_sex)$coefficients["Age:Sexfemale", "Pr(>|z|)"]
+age_male = summary(model_age_sex)$coefficients["Age:Sexmale", "Pr(>|z|)"]
+
